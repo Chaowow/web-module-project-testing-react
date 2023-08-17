02717cacaccb0643b5ac5ffe836d635c9afc5295
Frankie <frankiejosefej@gmail.com>
2023-08-17 05:41:44 -0400

    met mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,70 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+import mockFetchShow from './../../api/fetchShow'
 
-test('renders without errors with no props', async () => { });
+jest.mock('./../../api/fetchShow');
 
-test('renders Show component when the button is clicked ', () => { });
+const exampleShowData = {
+    name: 'Name of show',
+    summary: 'Summary of show',
+    seasons: [
+        {
+            id: 1,
+            season: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 2,
+            season: 'Season 2',
+            episodes: []
+        }
+    ]
+};
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders without errors with no props', async () => {
+    render(<Display />);
+ });
+
+test('renders Show component when the button is clicked ', async () => {
+    mockFetchShow.mockResolvedValueOnce(exampleShowData);
+
+    render(<Display />);
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    const show = await screen.findByTestId('show-container');
+
+    expect(show).toBeInTheDocument();
+ });
+
+test('renders show season options matching your data when the button is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(exampleShowData);
+
+    render(<Display />);
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option')
+        expect(seasonOptions).toHaveLength(2);
+      });
+ });
+
+ test('displayFunc is called when the fetch button is pressed', async () => {
+    mockFetchShow.mockResolvedValueOnce(exampleShowData);
+    const displayFunc = jest.fn();
+
+    render(<Display displayFunc={displayFunc}/>);
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled();
+    })
+ });
+
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,67 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-test('renders without errors', () => { });
+const exampleShowData = {
+    name: 'Name of show',
+    summary: 'Summary of show',
+    seasons: [
+        {
+            id: 1,
+            season: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 2,
+            season: 'Season 2',
+            episodes: []
+        }
+    ]
+};
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => {
+    render(<Show show={exampleShowData} selectedSeason={'none'}/>);
+ });
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} />);
+    const loading = screen.getByTestId('loading-container');
 
-test('handleSelect is called when an season is selected', () => { });
+    expect(loading).toBeInTheDocument();
+ });
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('renders same number of options seasons are passed in', () => {
+    render(<Show show={exampleShowData} selectedSeason={'none'}/>);
+    const seasonOptions = screen.queryAllByTestId('season-option');
+
+    expect(seasonOptions).toHaveLength(2);
+ });
+
+test('handleSelect is called when an season is selected', async () => {
+    const handleSelect = jest.fn();
+
+    render(<Show show={exampleShowData} selectedSeason={'none'} handleSelect={handleSelect} />);
+    const select = screen.getByLabelText(/select a season/i);
+
+    userEvent.selectOptions(select, ['2']);
+    
+    await waitFor(() => {
+        expect(handleSelect).toBeCalledTimes(1);
+      });
+ });
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={exampleShowData} selectedSeason={'none'} />);
+    let episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={exampleShowData} selectedSeason={1} />);
+    episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).toBeInTheDocument();
+ });
+
+
